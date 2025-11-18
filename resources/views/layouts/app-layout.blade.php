83accd110ef909887bfa6d80c06848ce151f54be
Abdelmawla1 <mahmoud.sayed.abdelmawla@gmail.com>
2025-11-18 10:45:15 +0200

    Add AppLayout component as the base layout

---
+++ resources/views/layouts/app-layout.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>{{ config('app.name', 'Laravel') }}</title>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet"/>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
+
+    </head>
+    <body>
+
+        <div class="min-h-screen bg-gray-100">
+            <main>
+                {{ $slot }}
+            </main>
+        </div>
+
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js"></script>
+    </body>
+</html>

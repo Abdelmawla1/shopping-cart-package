3e09c46b80123c873cf77dab2e9c665ef407f261
Abdelmawla1 <mahmoud.sayed.abdelmawla@gmail.com>
2025-11-18 14:58:58 +0200

    Fixing Bootstrap and design

--- resources/views/shop.blade.php
+++ resources/views/shop.blade.php
@@ -2,13 +2,16 @@
     <div class="py-12">
         <div class="max-w-7x1 mx-auto sm:px-6 lg:px-8">
             <div class="row">
-                <div class="col-md-4 mt-2">
-                    <img class="card-img-top" src="" alt="">
-                    <div class="card-body">
-                        <h1>Product Name</h1>
-                        <h1>Product Price</h1>
-                        <p>Product Description</p>
-                        <a href="#" class="btn- btn-success mt-3">Add To Cart</a>
+                <div class="col-md-3 mt-2">
+                    <div class="card" style="width: 18rem;">
+
+                        <img class="card-img-top" src="" alt="">
+                        <div class="card-body">
+                            <h1 class="card-title" style="font-size: 20px;">Product Name</h1>
+                            <h1 class="card-title" style="font-size: 20px;">Product Price</h1>
+                            <p class="card-text">Product Description</p>
+                            <a href="#" class="btn btn-success mt-3">Add To Cart</a>
+                        </div>
                     </div>
                 </div>
             </div>

eadcf2238709320a0ec927df72e16b6b7600f0e3
Abdelmawla1 <mahmoud.sayed.abdelmawla@gmail.com>
2025-11-18 11:13:46 +0200

    Add shop page

---
+++ resources/views/shop.blade.php
@@ -0,0 +1,17 @@
+<x-app-layout>
+    <div class="py-12">
+        <div class="max-w-7x1 mx-auto sm:px-6 lg:px-8">
+            <div class="row">
+                <div class="col-md-4 mt-2">
+                    <img class="card-img-top" src="" alt="">
+                    <div class="card-body">
+                        <h1>Product Name</h1>
+                        <h1>Product Price</h1>
+                        <p>Product Description</p>
+                        <a href="#" class="btn- btn-success mt-3">Add To Cart</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>

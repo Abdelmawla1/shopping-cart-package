1128bf8bbca96c6b19ae7a3b3edba84c349ce688
Abdelmawla1 <mahmoud.sayed.abdelmawla@gmail.com>
2025-11-19 14:40:06 +0200

    Add routes for shop and cart

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
    return view('welcome');
 });
+
+Route::get('shop', [CartController::class,'shop'])->name('shop');
+Route::get('cart', [CartController::class,'cart'])->name('cart');

36be06207e61e1a4f0e7b01d7b2f679737102089
Abdelmawla1 <mahmoud.sayed.abdelmawla@gmail.com>
2025-11-19 14:28:03 +0200

    Add database seeder for products

---
+++ database/seeders/ProductSeeder.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ProductSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    { 
+        $products = [
+            [
+            'image' => 'product1',
+            'name' => 'Head phone', 
+            'description' => Str::random(50), 
+            'price' => '75.00'
+            ],
+            [
+            'image' => 'product2',
+            'name' => 'Sun glass', 
+            'description' => Str::random(70), 
+            'price' => '19.99'
+            ],
+            [
+            'image' => 'product3',
+            'name' => 'Analog watch', 
+            'description' => Str::random(30), 
+            'price' => '10.99'
+            ],
+            [
+            'image' => 'product4',
+            'name' => 'Digital watch', 
+            'description' => Str::random(20), 
+            'price' => '7.50'
+        ]
+        ];
+        foreach($products as $product){
+            DB::table('products')->insert($product);
+        }
+    }
+}

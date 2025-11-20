7f4847562bcbc321fef9d37db901c58873693459
Abdelmawla1 <mahmoud.sayed.abdelmawla@gmail.com>
2025-11-20 15:39:14 +0200

    Add missing use statement for DB Facade

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Str;
 
 class ProductSeeder extends Seeder
@@ -12,30 +13,30 @@ class ProductSeeder extends Seeder
      * Run the database seeds.
      */
     public function run(): void
-    { 
+    {
         $products = [
             [
             'image' => 'product1',
-            'name' => 'Head phone', 
-            'description' => Str::random(50), 
+            'name' => 'Head phone',
+            'description' => Str::random(50),
             'price' => '75.00'
             ],
             [
             'image' => 'product2',
-            'name' => 'Sun glass', 
-            'description' => Str::random(70), 
+            'name' => 'Sun glass',
+            'description' => Str::random(70),
             'price' => '19.99'
             ],
             [
             'image' => 'product3',
-            'name' => 'Analog watch', 
-            'description' => Str::random(30), 
+            'name' => 'Analog watch',
+            'description' => Str::random(30),
             'price' => '10.99'
             ],
             [
             'image' => 'product4',
-            'name' => 'Digital watch', 
-            'description' => Str::random(20), 
+            'name' => 'Digital watch',
+            'description' => Str::random(20),
             'price' => '7.50'
         ]
         ];

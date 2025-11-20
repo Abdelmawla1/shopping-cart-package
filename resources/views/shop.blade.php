e027c74e8fc1152e1af1fac7b5f3355ea8cc59ea
Abdelmawla1 <mahmoud.sayed.abdelmawla@gmail.com>
2025-11-20 16:58:13 +0200

    Show images for all products on shop page

--- resources/views/shop.blade.php
+++ resources/views/shop.blade.php
@@ -3,21 +3,22 @@
         <div class="max-w-7x1 mx-auto sm:px-6 lg:px-8">
             <div class="row">
                 @if(count($products) > 0)
-                @foreach($products as $product)
-                <div class="col-md-3 mt-2">
-                    <div class="card" style="width: 18rem;">
+                    @foreach($products as $product)
+                        <div class="col-md-3 mt-2">
+                            <div class="card" style="width: 18rem;">
 
-                        <img class="card-img-top" src="" alt="">
-                        <div class="card-body">
-                            <h1 class="card-title" style="font-size: 20px;">{{ $product->name }}</h1>
-                            <h1 class="card-title" style="font-size: 20px;">{{ $product->price }}</h1>
-                            <p class="card-text">{{ $product->description }}</p>
-                            <a href="#" class="btn btn-success mt-3">Add To Cart</a>
+                                <img class="card-img-top"
+                                     src="{{ asset("assets/images/$product->image") }}" alt="">
+                                <div class="card-body">
+                                    <h1 class="card-title" style="font-size: 20px;">{{ $product->name }}</h1>
+                                    <h1 class="card-title" style="font-size: 20px;">{{ $product->price }}</h1>
+                                    <p class="card-text">{{ $product->description }}</p>
+                                    <a href="#" class="btn btn-success mt-3">Add To Cart</a>
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                </div>
                     @endforeach
-                    @endif
+                @endif
             </div>
         </div>
     </div>

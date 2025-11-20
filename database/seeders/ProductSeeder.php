03bd884e2bce82be4290e3394d4b850e409c8a8d
Abdelmawla1 <mahmoud.sayed.abdelmawla@gmail.com>
2025-11-20 15:12:56 +0200

    Add missing use statement for ProductSeeder for Str

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 class ProductSeeder extends Seeder
 {
